d97049d57e44a4439dcbf354b1d29f10b20f37ba
Erick Jara <erick.jara@unl.edu.ec>
2020-12-17 23:34:12 -0500

    first commit

---
+++ Models/Objeto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapeoTareaPracticas.Models
+{
+    public class Objeto
+    {
+        private string doctype;
+
+        private string userid;
+
+        public string id { get; set; }
+
+        private string name { get; set; }
+
+        private string Lastname { get; set; }
+
+        private string country { get; set; }
+
+        private string phone { get; set; }
+
+        private string address { get; set; }
+
+        private string email { get; set; }
+
+        private string date { get; set; }
+
+        private string dni { get; set; }
+
+        private string postalcode { get; set; }
+
+        public void SetDoctype(string doc)
+        {
+            this.doctype = doc;
+        }
+        
+        public string GetDoctype()
+        {
+            return this.doctype;
+        }
+
+        public Objeto ()
+        {
+
+        }
+
+        public Objeto(string id, string name, string lastName, string country, string phone, string address, string email, string date, string dni, string postalcode)
+        {
+            this.id = id;
+            this.name = name;
+            this.Lastname = lastName;
+            this.country = country;
+            this.phone = phone;
+            this.address = address;
+            this.email = email;
+            this.date = date;
+            this.dni = dni;
+            this.postalcode = postalcode;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using SpreadsheetLight;
+using MapeoTareaPracticas.Models;
+
+namespace MapeoTareaPracticas
+{
+    class Program
+    {
+        public static string ruta = @"D:\Respaldo\ASP.NET\MapeoTareaPracticas\";
+        public static List<Object[]> list = new List<Object[]>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GetExcelFile();
+            //ReadXML();
+            
+        }
+
+        public static void GetExcelFile()
+        {
+            //Ruta del fichero Excel
+            string filePath = ruta + "Ejemplo.xlsx";
+
+            SLDocument sl = new SLDocument(filePath);
+
+            int fileRow = 1; // fila desde donde empieza [Excel toma encuenta desde 1]
+
+            List<int> camposValidos = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                // almacena la posicion del campo que es valido de acuerso al esquema XML
+                if (!string.IsNullOrEmpty(Mapeo(sl.GetCellValueAsString(1, i + 1)))) { 
+                    camposValidos.Add(i);
+                    Console.Write(Mapeo(sl.GetCellValueAsString(1, i + 1)) + ", ");
+                }
+            }
+            Console.WriteLine("");
+
+            fileRow = 2; // empezar a leer desde la segunda fila
+            while (!string.IsNullOrEmpty(sl.GetCellValueAsString(fileRow, 1)))
+            {
+                Object[] o = new Object[10];
+                // leer datos de excel
+                Objeto obj = new Objeto();
+                foreach (var i in camposValidos)
+                {
+                    if (!string.IsNullOrEmpty(Mapeo(sl.GetCellValueAsString(1, i + 1))))
+                    {
+                        o[i] = sl.GetCellValueAsString(fileRow, i + 1);
+                    }
+
+                    
+                }
+
+                list.Add(o);
+
+                fileRow++; 
+            }
+
+            // presentar contenido del archivo siguiendo el esquema
+            for (int x = 0; x < list.Count; x++)
+            {
+                foreach (var i in camposValidos)
+                {
+                    if (!string.IsNullOrEmpty((string)list[x][i]))
+                    {
+                        Console.Write(list[x][i] + ", ");
+                    }
+                    else
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public static string Mapeo(string nameCol)
+        {
+            //aqui cargamos el documento con XDocument
+            XDocument documento = XDocument.Load(ruta + "schema.xml");
+
+            //            desde nombre //en de donde //descendants el nombre de la etiqueta //selecciona cual
+            var esquema = from sche in documento.Descendants("Schema") select sche;  //ahora en esta variable tenemos todo el esquema
+            //obtenemos el valor almacenado en la etiqueta document del esquema, la primera o default porque siempre llegara una
+            string doctype = esquema.Elements("document").FirstOrDefault().Value;
+            //Console.WriteLine(doctype);
+            //obtenemos el valor almacenado en la etiqueta user del esquema
+            string userid = esquema.Elements("user").FirstOrDefault().Value;
+            //Console.WriteLine(userid);
+
+            var columnas = from colu in documento.Descendants("columns") select colu;  //ahora en esta variable tenemos todas las columas
+            //cada elemento dentro del esquema es un XElement, recorremos con foreach para obtener el documento
+
+            foreach (XElement elemento in columnas.Elements("column"))
+            {
+                if (nameCol == elemento.Element("from").Value)
+                {
+                    return elemento.Element("to").Value;
+                }
+            }
+            return null;
+
+        }
+
+        public static void ReadXML()
+        {
+            //aqui cargamos el documento con XDocument
+            XDocument documento = XDocument.Load(ruta + "schema.xml");
+
+            //            desde nombre //en de donde //descendants el nombre de la etiqueta //selecciona cual
+            var esquema = from sche in documento.Descendants("Schema") select sche;  //ahora en esta variable tenemos todo el esquema
+            //obtenemos el valor almacenado en la etiqueta document del esquema, la primera o default porque siempre llegara una
+            string doctype = esquema.Elements("document").FirstOrDefault().Value;
+            Console.WriteLine(doctype);
+            //obtenemos el valor almacenado en la etiqueta user del esquema
+            string userid = esquema.Elements("user").FirstOrDefault().Value;
+            Console.WriteLine(userid);
+
+            var columnas = from colu in documento.Descendants("columns") select colu;  //ahora en esta variable tenemos todas las columas
+            //cada elemento dentro del esquema es un XElement, recorremos con foreach para obtener el documento
+
+            foreach (XElement elemento in columnas.Elements("column"))
+            {
+                Console.WriteLine(elemento.Element("to").Value);
+
+                Console.WriteLine(elemento.Element("from").Value);
+            }
+        }
+    }
+}
